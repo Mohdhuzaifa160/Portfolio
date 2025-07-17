e8a918b03ac902b8d21a37f6f751af64bf468b8d
Mohd Huzaifa <mohdhuzaifa160@gmail.com>
2025-07-18 02:31:40 +0530

    Initial portfolio upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed("#typed-text", {
+    strings: ["Mohd Huzaifa", "a Web Developer and AI Enthusiast"],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 2000,
+    startDelay:500,
+    loop: true
+  });
+});
